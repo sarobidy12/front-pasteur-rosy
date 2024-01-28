c31666362320435fc704200540ddf73fc4c1ef52
sarobidy12 <sarobidyalliance@outlook.com>
2024-01-28 13:59:43 +0300

    fix error deploie

--- next.config.js
+++ next.config.js
@@ -4,13 +4,7 @@ const nextConfig = {
     domains: ["localhost", "img.icons8.com", "back-end-cepd.onrender.com"],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  env: {
-    urlApi: process.env.API_BACK,
-    urlApiSocket: process.env.SOCKET,
   },
 };
 
